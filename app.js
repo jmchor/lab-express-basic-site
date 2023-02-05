7296e6c51f6d09cb538b26773eac7c110e942a76
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-05 11:52:38 +0100

    Edit app.js, add routes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.sendFile('views/index.html', { root: __dirname });
+});
+app.get('/about', (req, res) => {
+	res.sendFile('views/about.html', { root: __dirname });
+});
+app.get('/work', (req, res) => {
+	res.sendFile('views/work.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`🏃‍ on port ${port}`));
